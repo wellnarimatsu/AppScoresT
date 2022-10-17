416770a3af284ff1736849c9caa5a38a3e3daee3
wellison <wellison.narimatsu@outlook.com>
2022-10-17 12:30:40 -0300

    Inserindo Lógica Placar de Taco

--- app/src/main/java/br/com/appscorest/ui/Taco.kt
+++ app/src/main/java/br/com/appscorest/ui/Taco.kt
@@ -12,10 +12,58 @@ class Taco : AppCompatActivity() {
         ActivityTacoBinding.inflate(layoutInflater)
 
     }
+
+    private var pntTaco1 = 0
+    private var pntTaco2 = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         title="Taco"
+
+
+
+        binding.tacoCenter.setOnClickListener {
+
+            pntTaco1 = 0
+            binding.tacoPntTime1.text = pntTaco1.toString()
+
+            pntTaco2 = 0
+            binding.tacoPntTime2.text = pntTaco2.toString()
+        }
+
+        binding.tacoMaisTime1.setOnClickListener {
+            pntTaco1++
+
+            binding.tacoPntTime1.text = pntTaco1.toString()
+
+
+        }
+
+        binding.tacoMenosTime1.setOnClickListener {
+            pntTaco1--
+            binding.tacoPntTime1.text = pntTaco1.toString()
+        }
+
+
+
+
+        //TIME 2
+
+        binding.tacoMaisTime2.setOnClickListener {
+            pntTaco2++
+
+            binding.tacoPntTime2.text = pntTaco2.toString()
+
+
+        }
+
+        binding.tacoMenosTime2.setOnClickListener {
+            pntTaco2--
+            binding.tacoPntTime2.text = pntTaco2.toString()
+        }
+
+
     }
 }
